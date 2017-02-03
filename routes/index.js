1251cfbe3bda502183d336c0a990cc0a8f9d4161
aaujla55 <amandeep99555@gmail.com>
2017-02-03 13:09:49 -0500

    Basic 5 page structure created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET aboutMe page. */
+router.get('/aboutMe', function(req, res, next)
+{
+ res.render('aboutMe',{title: 'About Me'});
+});
+
+/* GET contactMe page */
+router.get('/contactMe',function(req, res, next)
+{
+res.render('contactMe',{title: 'Contact Me'});
+});
+
+/* GET projects page */
+router.get('/projects', function(req, res, next)
+{
+res.render('projects', {title: 'Projects'});
+});
+
+/*GET services page */
+router.get('/services', function(req, res, next)
+{
+res.render('services', {title : 'Services'});
 });
 
 module.exports = router;
