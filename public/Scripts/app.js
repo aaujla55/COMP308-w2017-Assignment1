d8a4152ab7f8c2a15cb70003a9a9c2fe701551cf
aaujla55 <amandeep99555@gmail.com>
2017-02-10 19:43:54 -0500

    Navigation property change

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,7 +1,17 @@
-var colors = ['#b94046', '#FBA2A7','#961f52' ];
-$(document).ready(function(){
-$(".gallery-delayclass").css("background-color", "#424242"); 
 
-});
+ $(document).ready(function(){
+      $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 250) { // check if user scrolled more than 50 from top of the browser window
+          $(".topnav").css("background-color", "#000"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+          $(".topnav").css("opacity", "0.6");
+        } else {
+          $(".topnav").css("background-color", "transparent"); // if not, change it back to transparent
+          $(".topnav").css("opacity", "1");
+        }
+      });
+    });
 
 
+    function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+    }
